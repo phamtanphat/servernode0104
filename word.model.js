48802010e3a80c4dcf19c7c46d7347afee59aa19
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-24 18:54:48 +0700

    a

--- word.model.js
+++ word.model.js
@@ -13,7 +13,17 @@ const Word = mongoose.model('Word', {
 // .then(words => console.log(words));
 
 //INSERT
-const word = new Word({en : 'One' , vn : 'Mot'});
+const word = new Word({en : 'Three' , vn : 'Ba'});
 word.save()
 .then(w => console.log(w))
 .catch(error => console.log(error.message));
+
+//UPDATE
+// Word.findByIdAndUpdate('5cc04c2bade01025c43c9ff6',{isMemorized : false} ,{new : true})
+// .then(w => console.log(w))
+// .catch(error => console.log(error.message));
+
+//DELETE
+// Word.findByIdAndRemove('5cc04c2bade01025c43c9ff6')
+// .then(w => console.log(w))
+// .catch(error => console.log(error.message));
