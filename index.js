8d4f716c0e0f5dea6529e24d0dca1f50b0fdeb14
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-24 19:23:06 +0700

    a

--- index.js
+++ index.js
@@ -15,9 +15,29 @@ app.post('/word' ,(req,res) =>{
     word.save()
     .then(w => {
         if(!w) throw new Error('DUPLICATE_VALUE')
-        return res.send({success : true , word : w})
+        res.send({success : true , word : w})
     })
     .catch(error => res.send({success : false , message : error.message}));
 })
+app.put('/word/:_id',(req,res) => {
+    const {_id} = req.params;
+    const{isMemorized} = req.body;
+    Word.findByIdAndUpdate(_id,{isMemorized} ,{new : true})
+    .then(w => {
+        if(!w) throw new Error("EMPTY_WORD");
+        res.send({success : true , word : w});
+    })
+    .catch(error => res.send({success : false , message : error.message}));
+});
+app.delete('/word/:_id' , (req,res)=>{
+    const{_id} = req.params;
+    Word.findByIdAndRemove(_id)
+    .then(w => {
+        if(!w) throw new Error("EMPTY_WORD");
+        res.send({success : true , word : w});
+    })
+    .catch(error => res.send({success : false , message : error.message}));
+
+});
 
 app.listen('4000' ,() => console.log("Server Started"));
