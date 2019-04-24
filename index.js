9dd45088b0ace4ca07abbf0f87dee46f2c239c5a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-04-24 17:54:20 +0700

    a

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const {json} = require('body-parser');
 
 const app = express();
+
+app.use(json());
 
 app.get('/cong/:a/:b' , (req , res) => {
     const {a , b} = req.params;
@@ -16,6 +19,15 @@ app.get('/cong/:a/:b' , (req , res) => {
     })
     tinhtoan.then(ketqua => console.log(ketqua))
     .catch(error => console.log(error));
-})
+});
+
+app.put("/chia" , (req,res) =>{
+    const {a , b} = req.body;
+    if(isNaN(a) || isNaN(b)){
+        return res.send({success : false , message : 'INVALID_TYPE'});
+    }
+    const ketqua = +a + +b;
+    res.send({success : true , ketqua});
+});
 
 app.listen('4000' ,() => console.log("Server Started"));
